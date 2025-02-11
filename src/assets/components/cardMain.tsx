f9308606e06341ff51fb596c1e856ee64d89be1e
ahmadrido <ahmadridokamaludin.com>
2025-02-11 17:36:35 +0700

    update

--- src/assets/components/cardMain.tsx
+++ src/assets/components/cardMain.tsx
@@ -7,15 +7,15 @@ interface CardProps {
 
 const CardMain = ( {title, description, url, Img}: CardProps) => {
     return (
-        <div className=" bg-white mt-5 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <div className=" mt-5 border rounded-lg shadow-sm bg-gray-800 border-gray-700">
                 <a href={url} target="_blank">
                     <img className="rounded-t-lg w-full max-h-56" src={Img} alt="" />
                 </a>
                 <div className="p-5">
                     <a href={url} target="_blank">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-1">{title}</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white line-clamp-1">{title}</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3">{description}</p>
+                    <p className="mb-3 font-normal text-gray-400 line-clamp-3">{description}</p>
                     <a href={url} target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Baca Selengkapnya
                         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
