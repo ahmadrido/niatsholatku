ebbd0b47782bdf3ba0f99e9b295102a7002a3d46
ahmadrido <ahmadridokamaludin.com>
2025-02-11 17:58:41 +0700

    update

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -93,7 +93,7 @@ const Navigation = () => {
         onChange={(e) => setSearchQuery(e.target.value)} className="block w-76 md:w-96 p-2 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autoComplete='off' placeholder="Cari..." />
 
       {searchQuery && (
-        <ul className="absolute text-black w-80 mt-2 rounded-md shadow-lg max-h-60 overflow-y-auto">
+        <ul className="absolute text-black w-80 mt-2 rounded-md shadow-lg max-h-60 overflow-y-auto bg-white">
           {filteredResults.length > 0 ? (
             filteredResults.map((item, index) => (
               <li
