136575571608fb7fd6f2b4e423f16d86dcfd908f
ahmadrido <ahmadridokamaludin.com>
2025-02-11 17:56:07 +0700

    update

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -29,7 +29,7 @@ const Navigation = () => {
             Indonesia
           </button>
         
-          <div className="z-50 hidden my-4 text-base list-none divide-y divide-gray-100 rounded-lg shadow-sm bg-gray-700" id="language-dropdown-menu">
+          <div className="z-50 hidden my-4 text-base list-none divide-y divide-gray-100 rounded-lg shadow-sm bg-white" id="language-dropdown-menu">
             <ul className="py-2 font-medium" role="none">
               <li>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
