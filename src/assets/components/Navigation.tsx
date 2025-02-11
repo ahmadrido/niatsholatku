2bb36559ba109d3f94715a9f1aeba0457b02bf58
ahmadrido <ahmadridokamaludin.com>
2025-02-11 17:23:23 +0700

    Update navbar

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation = () => {
     window.location.reload();
   };
     return ( 
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow shadow-gray-500">
+      <nav className="border-gray-200 bg-gray-900 shadow shadow-gray-500">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="/images/logo.png" className="h-8" alt="NiatSholatku 2025 Logo" />
